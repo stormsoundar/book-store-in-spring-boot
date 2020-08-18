3504cf98b601087ab4d3e8f01a00cc21a486240e
Soundararajan M <soundarit1995@gmail.com>
2020-08-18 23:17:58 +0530

    Update BookRepository.java

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -13,39 +13,12 @@ import java.util.Set;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-//      Optional<Book> findById(Long bookId);
-   // Book findById(Long bookId);
-
-//    List<Book> findAllByYearOfPublication(Integer yop);
-//    List<Book> findAllByYearOfPublicationIn(Set<Integer> yop);
 
     List<Book> findAllByYearOfPublicationInAndBookType(Set<Integer> yop, String bookType);
 
-    // native query method 1
-//    String rawQuery = "select * from book where year_of_publication IN ?1";
-//
-//    @Query(nativeQuery = true, value = rawQuery)
-//    List<Book> findAllByYearOfPublicationIn(Set<Integer> yop);
-
-    // native query method 2
     String rawQuery = "select * from book where year_of_publication IN :yop";
 
     @Query(nativeQuery = true, value = rawQuery)
     List<Book> findAllByYearOfPublicationIn(@Param("yop") Set<Integer> yop);
 
-
-
-//    Book delete(Integer bookId);
-
-
-//    Book deleteById(Integer bookId);
-
-
-    // Save to db
-
-    // Update
-
-    // Fetch
-
-    //Delete
 }
